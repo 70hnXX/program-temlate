7186c884da1a7c811aec0e6bf84db7922cbf3f96
wuyangjiacuo <wuyangjiacuo@gmail.co>
2020-07-01 15:31:55 +0800

    更新选项

--- command/init.js
+++ command/init.js
@@ -3,15 +3,16 @@
 const exec = require("child_process").exec;
 const co = require("co");
 // const clone = require('git-clone')
+// const log = require('tracer').colorConsole()
 const prompt = require("co-prompt");
 const config = require("../templates");
 const inquirer = require('inquirer')
+// const shell = require('shelljs')
 // const chalk = require("chalk");
 const {
   chalk,
   execa,
   semver,
-
   log,
   warn,
   error,
@@ -33,6 +34,16 @@ module.exports = () => {
     // 处理用户输入
     // let tplName = yield prompt("Template name: ");
     // let projectName = yield prompt("Project name: ");
+    let name = ''
+    let branch = '' // 分支
+    // 项目名
+    let projectName = await inquirer.prompt({
+      name: 'projectName',
+      type: 'input',
+      message: '请输入项目名称:'
+    })
+    name = projectName.projectName
+    // 选择分支
     let framwork = await inquirer.prompt({
       name: 'framwork',
       type: 'list',
@@ -52,26 +63,64 @@ module.exports = () => {
         }
       ]
     })
-    let situation = await inquirer.prompt({
-      name: 'framwork',
-      type: 'list',
-      message: `选择使用场景:`,
-      choices: [
-        {
-          name: 'Vue',
-          value: 'vue'
-        },
-        {
-          name: 'React',
-          value: 'react'
-        }
-      ]
+    branch = framwork.framwork
+    // vue
+    if (framwork.framwork === 'vue') {
+      let situation = await inquirer.prompt({
+        name: 'situation',
+        type: 'list',
+        message: `选择使用场景:`,
+        choices: [
+          {
+            name: 'web-mobile',
+            value: '-app'
+          },
+          {
+            name: 'web-pc',
+            value: '-web'
+          },
+          {
+            name: 'web-admin',
+            value: '-admin'
+          }
+        ]
+      })
+      branch += situation.situation
+    }
+    // react
+    if (framwork.framwork === 'react') {
+      let situation = await inquirer.prompt({
+        name: 'situation',
+        type: 'list',
+        message: `选择使用场景:`,
+        choices: [
+          {
+            name: 'web-admin',
+            value: '-admin'
+          },
+          {
+            name: 'taro',
+            value: '-taro'
+          }
+        ]
+      })
+      branch += situation.situation
+    }
+    // 确认
+    // 项目名
+    let confirmConfig = await inquirer.prompt({
+      name: 'confirmConfig',
+      type: 'confirm',
+      message: '确认新建一个' + branch + '类型的项目?'
     })
-    let projectName = 'app'
-    let branch = 'cli'
-    let cmdStr = `git clone https://github.com/70hnXX/program-temlate.git && cd ${projectName} && git checkout ${branch}`;
-    // console.log(chalk.white("\n Start generating..."));
+    console.log(confirmConfig)
+    let cmdStr = `mkdir ${name} && git clone https://github.com/70hnXX/program-temlate.git && cd ${name} && git checkout ${branch}`;
     log(`✨  Creating project in projectName`)
+
+    // clone(`https://gitee.com/Johnwuyang/vicetone-cli-fontend.git`, pwd + `/${project}`, null, function () {
+    //     shell.rm('-rf', pwd + `/${project}/.git`)
+    //     log.info('done enjoy ╭(●｀∀´●)╯')
+    //   })
     exec(cmdStr, (error, stdout, stderr) => {
       if (error) {
         console.log(error);
