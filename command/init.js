f686be8ff8d7fe7e52b26679873672be28ef9edc
wuyangjiacuo <wuyangjiacuo@gmail.co>
2020-07-01 13:57:56 +0800

    添加新的包

--- command/init.js
+++ command/init.js
@@ -1,54 +1,84 @@
 "use strict";
 
 const exec = require("child_process").exec;
-
 const co = require("co");
-
+// const clone = require('git-clone')
 const prompt = require("co-prompt");
-
 const config = require("../templates");
+const inquirer = require('inquirer')
+// const chalk = require("chalk");
+const {
+  chalk,
+  execa,
+  semver,
 
-const chalk = require("chalk");
+  log,
+  warn,
+  error,
+  logWithSpinner,
+  stopSpinner,
+
+  hasGit,
+  hasProjectGit,
+  hasYarn,
+  hasPnpm3OrLater,
+  hasPnpmVersionOrLater,
+
+  exit,
+  loadModule
+} = require('@vue/cli-shared-utils')
 
 module.exports = () => {
-  co(function* () {
+  co(async function () {
     // 处理用户输入
-
-    let tplName = yield prompt("Template name: ");
-
-    let projectName = yield prompt("Project name: ");
-
-    let gitUrl;
-
-    let branch;
-
-    if (!config.tpl[tplName]) {
-      console.log(chalk.red("\n × Template does not exit!"));
-
-      process.exit();
-    }
-
-    gitUrl = config.tpl[tplName].url;
-
-    branch = config.tpl[tplName].branch;
-
-    // git命令，远程拉取项目并自定义项目名
-
-    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`;
-
-    console.log(chalk.white("\n Start generating..."));
-
+    // let tplName = yield prompt("Template name: ");
+    // let projectName = yield prompt("Project name: ");
+    let framwork = await inquirer.prompt({
+      name: 'framwork',
+      type: 'list',
+      message: `选择你想使用的前端框架:`,
+      choices: [
+        {
+          name: 'Vue',
+          value: 'vue'
+        },
+        {
+          name: 'React',
+          value: 'react'
+        },
+        {
+          name: 'MiniProgram',
+          value: 'wechat'
+        }
+      ]
+    })
+    let situation = await inquirer.prompt({
+      name: 'framwork',
+      type: 'list',
+      message: `选择使用场景:`,
+      choices: [
+        {
+          name: 'Vue',
+          value: 'vue'
+        },
+        {
+          name: 'React',
+          value: 'react'
+        }
+      ]
+    })
+    let projectName = 'app'
+    let branch = 'cli'
+    let cmdStr = `git clone https://github.com/70hnXX/program-temlate.git && cd ${projectName} && git checkout ${branch}`;
+    // console.log(chalk.white("\n Start generating..."));
+    log(`✨  Creating project in projectName`)
     exec(cmdStr, (error, stdout, stderr) => {
       if (error) {
         console.log(error);
-
         process.exit();
       }
-
       console.log(chalk.green("\n √ Generation completed!"));
-
       console.log(`\n cd ${projectName} && npm install \n`);
-
       process.exit();
     });
   });
